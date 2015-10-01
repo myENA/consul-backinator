0352c9b62ab3d8a1232aea463a16ef47e44e1932
Aaron Hurt <ahurt@anbcs.com>
2015-09-30 22:53:03 -0500

    consistent spacing

--- consul.go
+++ consul.go
@@ -12,24 +12,30 @@ type client struct {
 func buildClient(address, scheme, dc, token string) (*client, error) {
 	// build default config
 	config := api.DefaultConfig()
+
 	// overwrite address if needed
 	if address != "" {
 		config.Address = address
 	}
+
 	// overwrite scheme if needed
 	if scheme != "" {
 		config.Scheme = scheme
 	}
+
 	// overwrite dc if needed
 	if dc != "" {
 		config.Datacenter = dc
 	}
+
 	// overwrite token if needed
 	if token != "" {
 		config.Token = token
 	}
+
 	// build client
 	c, err := api.NewClient(config)
+
 	// return wrapped client
 	return &client{Client: c}, err
 }
@@ -40,14 +46,18 @@ func (c *client) getKeys(prefix string) ([]byte, int, error) {
 		AllowStale:        false,
 		RequireConsistent: true,
 	}
+
 	// get all keys
 	pairs, _, err := c.KV().List(prefix, opts)
+
 	// check error
 	if err != nil {
 		return nil, 0, err
 	}
+
 	// encode and return
 	data, err := json.Marshal(pairs)
+
 	// return
 	return data, len(pairs), err
 }
